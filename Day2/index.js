891afd34680f34cc155b0ddf6876216e2b568b1b
Aaryan Kumar <aaryan14032006@gmail.com>
2026-05-02 17:31:14 +0530

    splice done

--- Day2/index.js
+++ Day2/index.js
@@ -124,6 +124,11 @@ console.log(tally);
 //     return b(a(1, 2), c);
 // }
 
+// arr -> 1, 3, 4, 5, 6
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+console.log(months.splice(2, 3, 'Aaryan')); // Removes "February" and "March"
+console.log(months);
+
 
 
 
