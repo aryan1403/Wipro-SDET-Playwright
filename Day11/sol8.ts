017f4c1f4289a5972c68b6296918a85702cb8f55
Aaryan Kumar <aaryan14032006@gmail.com>
2026-05-13 14:56:13 +0530

    sol8

---
+++ Day11/sol8.ts
@@ -0,0 +1,26 @@
+function safeExecute<Args extends any[], T>(asyncFnc: (...args: Args) => Promise<T>) {
+    return async (...args: Args): Promise<T | null> => {
+        try {
+            return await asyncFnc(...args);
+        } catch (err) {
+            console.error(err);
+            return null;
+        }
+    }
+}
+
+const info = async (id: number): Promise<string> => {
+    if(id === -1) throw new Error("Invalid ID");
+    return `Data for ID: ${id}`
+}
+
+
+async function dryRun() {
+    const getData = safeExecute(info);
+    const res = await getData(10);
+    const res1 = await getData(0);
+    const res2 = await getData(-1);
+    console.log(res, res1, res2);
+}
+
+dryRun();
